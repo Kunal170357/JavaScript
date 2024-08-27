da74d92f5633eb2b950806135aab6fb1afce466b
kunal170357 <2002kunalborse@gmail.com>
2024-08-27 11:32:03 +0530

    commit done

---
+++ Operation.js
@@ -0,0 +1,19 @@
+console.log(2+2); // plus operator
+console.log(2-2); //minus operator
+console.log(2*2); // multiplication operator
+console.log(2**2); // power operation
+console.log(2/3); // divide operator
+console.log(2%3); // modulo operator
+
+console.log("1"+2 );
+console.log(2+"1");
+console.log("1" + 2+ 2); // output is string =  122
+console.log(1 + 2 + "3"); // output is 33  predefined rule in js
+
+
+
+
+
+
+
+
